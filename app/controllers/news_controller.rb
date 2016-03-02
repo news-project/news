fc6d09e752cf5488d3b7b976a09f053cd3ef197a
boramamohani <boramhan@gmail.com>
2016-03-03 00:41:09 +0900

    changed twitter query

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,7 +1,9 @@
 class NewsController < ApplicationController	
 	def index
-		#@tweets = $twitter.get_all_tweets('boramamohani')
-		@tweets = $twitter.search("감사", geocode: '37.527114,127.027861,100km', result_type: "mixed", lang: 'ko').take(20)
+		
+	#	@tweets = $twitter.search("감사", geocode: '37.527114,127.027861,100km', result_type: "mixed", lang: 'ko').take(20)
+		@tweets = $twitter.search("감사", result_type: "popular", lang: 'ko').take(20)
+	
 	end
 	
 end
