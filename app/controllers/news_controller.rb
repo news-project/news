c952e192d44239886ea876599c1130fe7e74d416
boramamohani <boramhan@gmail.com>
2016-03-06 17:40:30 +0900

    뉴스에 대한 짹짹이 100개_배포

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,9 +1,11 @@
 class NewsController < ApplicationController	
 	def index
 		
-	@q = "오늘"
-	#	@tweets = $twitter.search("감사", geocode: '37.527114,127.027861,100km', result_type: "mixed", lang: 'ko').take(20)
-	@tweets = $twitter.search(@q, result_type: "recent", lang: 'ko').take(100)
+		@q = "뉴스"
+		@howmany = 100
+		@qtype = "recent"
+		#	@tweets = $twitter.search("감사", geocode: '37.527114,127.027861,100km', result_type: "mixed", lang: 'ko').take(20)
+		@tweets = $twitter.search(@q, result_type: @qtype, lang: 'ko').take(@howmany)
 	
 	end
 	
